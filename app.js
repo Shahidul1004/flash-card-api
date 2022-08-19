7241861e0e9b999bf0bde289bf3837c07420ef23
Shahidul <sonysust@gmail.com>
2022-08-19 10:51:15 +0600

    vercel.json

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+  res.setHeader("Cache-Control", "s-max-age=5, stale-while-revalidate");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
   next();
 });
